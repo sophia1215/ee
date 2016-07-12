4a90701025d07e266c3e0fe025b84c3bc4b9ff32
sophia1215 <code.sophiawang@gmail.com>
2016-07-12 20:15:33 +0800

    Sublime format

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,21 +10,21 @@
 #
 
 class Post < ActiveRecord::Base
-	has_many :taggings
-	has_many :tags, through: :taggings
+  has_many :taggings
+  has_many :tags, through: :taggings
 
-	#Getter and Setter for all_tags vertial attribute
-	def all_tags=(names)
-	  self.tags = names.split(",").map do |name|
-	  	Tag.where(name: name.strip).first_or_create!
-	  end
-	end
+  #Getter and Setter for all_tags vertial attribute
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
 
-	def all_tags
-	  self.tags.map(&:name).join(", ")
-	end
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
 
-	def self.tagged_with(name)
-	  Tag.find_by_name!(name).posts
-	end
+  def self.tagged_with(name)
+    Tag.find_by_name!(name).posts
+  end
 end
